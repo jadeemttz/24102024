7d870f7d0dcb227e52edcf398fb9f35028fe5495
jadeemttz <jmartinezh2211@alumno.ipn.mx>
2024-10-24 16:47:54 -0600

    hola

--- java.js
+++ java.js
@@ -1,4 +1,4 @@
- let array = ["dato1", 1, Boolean];
+ /*let array = ["dato1", 1, Boolean];
  console.log(array.length);
  array.push("Nuevo Valor");
  console.log(array[1]);
@@ -6,17 +6,34 @@
  console.log(array[2]);
 
 //tabla del 5 hasta el 6 y fin del arreglo al final y  y lenght
-
+//declaramos el arreglo
 let resultados = [];
+//iteramos la multiplicaciòn para obtener la tabla completa del 1 al 10
 for ( var i=0; i<10; i++){
     producto=5*(i+1);
     //console.log(producto);
+    //Evaluamos si el producto es menor a 30 ya que es el ultimo valor que buscamos
     if (producto<=30){
+        //agregamos cuando ya se haya evaluado que es menor o igual a 30
         resultados.push(producto);
     }
 }
+//se imprime el arreglo completo tomando como base su longitud
 for ( var j=0; j<resultados.length; j++){
 console.log(resultados[j]);
 }
+//se imprime la ultima posicion del arreglo eliminandole uno a la longitud ya que inicia desde el 0
 console.log("Fin del arreglo ultima posicion: "+ resultados[resultados.length-1]);
+// se imprime la longitud
 console.log("Tamaño del arreglo: "+ resultados.length);
+
+let array = ["dato1", "dato2", "dato3"];
+array.push("dato4");
+console.log(array[3]);
+//posicion coma numero de datos a eliminar, inicia desde la posicion 2 y a partir de ahi elimina 2 datos
+// en este caso elimina a partir del dato3 y elimina 2 datos, o sea dato 3 y 4
+array.splice(2,1);
+console.log(array);
+
+//poder agregar tareas y que aparezcan abajo no se pueden agregar tareas vacias o con espacios y se pueden eliminar todas las tareas
+*/
